ccbf44cec912e20af4a51cd3ca6b16a35c94836f
Aleksandr Chubutkin <chubutkin.a@applecity.kz>
2019-03-28 22:58:44 +0600

    Remove plugins with build problems.

--- typings/cordova-typings.d.ts
+++ typings/cordova-typings.d.ts
@@ -1,2 +1,4 @@
-
 /// <reference path="..\.vscode\typings\cordova\cordova.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Device.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\FileSystem.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\BarcodeScanner.d.ts"/>
